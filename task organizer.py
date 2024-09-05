10459c1db8a2e3f582d797ec845036270de3539c
wilson-mbochi <wilson.mb91@gmail.com>
2024-09-05 03:07:09 -0400

    Update task organizer.py

--- task organizer.py
+++ task organizer.py
@@ -2,15 +2,15 @@
 mylist = ['read a chapter of Python by Eric Matthes', 'return amazon package', 'find a new recipe']
 # program loops 6 times
 for task in range(0, 6):
-    myChoice = input("I have a list of things to address. What do I want to do? "
-                     "\n\t(A)Look at list of things to do (B)Sleep til problems go away (C)Add more to list :")
-    if myChoice == "A":
+    myChoice = input("I have a list of things to address. What do I want to do? Chose a letter. "
+                     "\n\t(A)Look at list of things to do (B)Sleep til problems go away (C)Add more to list :").lower()
+    if myChoice == "a":
         print(f"{mylist} \nLet's do the last task first:")
         print(f"{mylist[-1]} ...Done\n")
         del mylist[-1]
-    elif myChoice == "B":
+    elif myChoice == "b":
         print("\nEnjoy a blissful nap!...Zzz...\n")
-    elif myChoice == "C":
+    elif myChoice == "c":
         moreThings = input("Add to list..")
         mylist.append(moreThings)
     else:
