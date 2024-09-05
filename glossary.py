b95108ce1810cd2ad38e72235e0635d153a4457e
wilson-mbochi <wilson.mb91@gmail.com>
2024-09-05 03:43:41 -0400

    Update glossary.py

--- glossary.py
+++ glossary.py
@@ -1,12 +1,14 @@
 # dictionary of glossary words
-pythonConcepts = {'variables': 'assign data/values to a keyword',
-                      'for loops': 'loop through a list, items or range of values and perform actions for duration specified',
-                      'conditionals': 'use boolean operators, if, elif, and else cases to test for true or false then perform actions',
-                      'dictionaries': 'store data/values to key-value pairs'}
+pythonConcepts = {'conditionals': 'use boolean operators, if, elif, and else cases to test for true or false then perform actions',
+                  'dictionaries': 'store data/values to key-value pairs',
+                  'for loops': 'loop through a list, items or range of values and perform actions for duration specified',
+                  'variables': 'assign data/values to a keyword'}
 for k, v in pythonConcepts.items():
     print(f"{k.upper()}")
 userRequest = input("\nSearch a concept above in the glossary for the definition: ").lower()
 for item in pythonConcepts.keys():
     if item in userRequest:
         print(f"\t{item.upper()}: {pythonConcepts[item].lower()}")
-else: print("sorry, not found!")
+        break
+else:
+    print("sorry, not found!")
