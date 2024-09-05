edd3c4fd78060a26cb4758e650b58d0fd4682f4a
wilson-mbochi <wilson.mb91@gmail.com>
2024-09-05 02:24:46 -0400

    Update order_pizza.py

--- order_pizza.py
+++ order_pizza.py
@@ -1,11 +1,15 @@
 # nested lists within a dictionary for a pizza ordering system
+print("we have a sale on thin crust pizzas! Choose your toppings!")
 toppings = []
 for order in range(5):
     remaining = 5 - order
-    userRequest = input(f"{remaining} topping choices (enter 1 at a time): ")
+    userRequest = input(f"{remaining} more topping choices (enter 1 at a time): ")
     toppings.append(userRequest)
-pizza = {'toppings': toppings, 'crust': 'thin', 'sauce': 'ranch'}
-print(f"\nyou have ordered a {pizza['crust']} crust pizza with {pizza['sauce']} drizzle on top."
+sauce = input(f"Enter your favorite sauce: ")
+pizza = {'toppings': toppings, 'crust': 'thin'}
+print(f"\nyou have ordered a promotional {pizza['crust']} crust pizza with {sauce} drizzle on top."
       f"\nIt includes the following toppings: ")
 for item in pizza['toppings']:
     print(f"\t{item}")
+
+print("sending order off to Papa RAM's store...")
